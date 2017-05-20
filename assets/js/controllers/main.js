73ebd919dbda155fa05c090bdde6a87018676540
Widi Anggryanto <Home@Widis-MacBook-Pro.local>
2017-05-20 16:30:36 +0800

    [Library]: add requirejs, jquery and material design lite

---
+++ assets/js/controllers/main.js
@@ -0,0 +1,11 @@
+define(
+    'controllers/main',
+    [],
+    function() {
+        console.info('Main controller');
+
+        var controller = 'main controller';
+
+        return controller;
+    }
+);
---
+++ assets/js/initialize.js
@@ -0,0 +1,29 @@
+/**
+ * global require
+ */
+
+requirejs.config({
+    baseUrl: 'assets',
+    paths: {
+        // Vendors libraries
+        'jquery': 'bower_components/jquery/dist/jquery.min',
+        'mdl': 'bower_components/material-design-lite/material.min',
+
+        // Our own JS
+        'controllers': 'js/controllers',
+        'models': 'js/models'
+    }
+});
+
+requirejs(
+    [
+        'mdl', 'jquery',
+        'controllers/main'
+    ],
+    function(
+        mdl, $,
+        main
+    ) {
+        console.info('Initialized', mdl, $, main);
+    }
+)
