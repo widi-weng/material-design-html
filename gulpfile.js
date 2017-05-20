f576658a1e8888494b97360fc2c00e6f8544c81b
Widi Anggryanto <Home@Widis-MacBook-Pro.local>
2017-05-20 23:19:28 +0800

    [Node]: add package.json, gulp, gulp-sass, node-sass, gulp-sourcemaps and automate css generation

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+ 
+gulp.task('default', function () { 
+    console.log('Running Gulp...');
+    return;
+});
+
+gulp.task('sass', function () {
+    console.log('SASS: Produce CSS and source maps');
+    return gulp.src('assets/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('assets/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+    console.log('SASS Watch: Watching changes in SCSS files');
+    gulp.watch('assets/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('develop', ['default', 'sass', 'sass:watch'], function() {
+  // Do stuff
+});
